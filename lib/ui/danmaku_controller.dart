96bfc2a47302692fb30c6db3b9b7cbf0c93127de
o0kam1 <duzhaokun2@gmail.com>
2025-04-08 22:12:37 +0800

    clean code

--- lib/ui/danmaku_controller.dart
+++ lib/ui/danmaku_controller.dart
@@ -9,63 +9,83 @@ import '../model/danmakus.dart';
 
 class DanmakuController {
   static const int poolUndefined = -1;
+
   final DanmakuWidget widget;
   final dynamic state;
-  DanmakuController({
-    required this.widget,
-    required this.state,
-});
+
+  DanmakuController({required this.widget, required this.state});
+
   var showingDanmakus = <ShowingDanmakuInfo>[];
-  double get conductedTimeMs => conductedTimeUs / 1_000;
+
+  /// 进行时间毫秒
+  get conductedTimeMs => conductedTimeUs / 1_000;
+
+  /// 进行时间微秒
+  /// 不要直接 set 这个值 使用 [seekTo]
   var conductedTimeUs = 0;
+
   final danmakus = <int, Danmakus>{};
   var drawDebugInfo = false;
-  var debugStyle = TextStyle(
-    color: const Color(0xFFFFFFFF),
-    fontSize: 20,
-  );
+  var debugStyle = TextStyle(color: const Color(0xFFFFFFFF), fontSize: 20);
+
+  /// -1: 可以倒放
   var speed = 1.0;
-  var drawPaused = true;
-  var _period = 16;
+
+  /// 弹幕绘制周期 1000ms / fps 得到
   int get period => _period;
+
+  get drawPaused => _drawPaused;
+
+  var _drawPaused = true;
+  var _period = 16;
+
   set period(int value) {
     _period = value;
     state.launcherTimer(period: value);
   }
-  var shouldDrawOneTime = false;
-void pause() {
-  drawPaused = true;
-}
-void resume() {
-  drawPaused = false;
-}
-void seekTo(int timeMs) {
-  conductedTimeUs = timeMs * 1_000;
-  drawOneTime();
-}
 
-void start({int timeMs = 0}) {
-  conductedTimeUs = timeMs * 1_000;
-  resume();
-}
-
-void drawOneTime() {
-  state.setState(() {});
-}
-
-void addDanmakus(Danmakus danmakus, {int pool = DanmakuController.poolUndefined}) {
-  if (this.danmakus[pool] == null) {
-    this.danmakus[pool] = danmakus;
-  } else {
-    this.danmakus[pool]!.addAll(danmakus);
+  void pause() {
+    _drawPaused = true;
   }
-}
 
-void addDanmaku(Danmaku danmaku, {int pool = DanmakuController.poolUndefined}) {
-  if (danmakus[pool] == null) {
-    danmakus[pool] = Danmakus();
+  void resume() {
+    _drawPaused = false;
   }
-  danmakus[pool]!.add(danmaku);}
+
+  void seekTo(int timeMs) {
+    conductedTimeUs = timeMs * 1_000;
+    drawOneTime();
+  }
+
+  void start({int timeMs = 0}) {
+    conductedTimeUs = timeMs * 1_000;
+    resume();
+  }
+
+  void drawOneTime() {
+    state.setState(() {});
+  }
+
+  void addDanmakus(
+    Danmakus danmakus, {
+    int pool = DanmakuController.poolUndefined,
+  }) {
+    if (this.danmakus[pool] == null) {
+      this.danmakus[pool] = danmakus;
+    } else {
+      this.danmakus[pool]!.addAll(danmakus);
+    }
+  }
+
+  void addDanmaku(
+    Danmaku danmaku, {
+    int pool = DanmakuController.poolUndefined,
+  }) {
+    if (danmakus[pool] == null) {
+      danmakus[pool] = Danmakus();
+    }
+    danmakus[pool]!.add(danmaku);
+  }
 
   void removeAllDanmakus() {
     danmakus.clear();
@@ -81,7 +101,9 @@ void addDanmaku(Danmaku danmaku, {int pool = DanmakuController.poolUndefined}) {
       }
     });
     if (kDebugMode) {
-      print("buildCache: $count, ${DateTime.now().millisecondsSinceEpoch - startTime}ms");
+      print(
+        "buildCache: $count, ${DateTime.now().millisecondsSinceEpoch - startTime}ms",
+      );
     }
   }
 
